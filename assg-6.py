4d87685fb94916d2ff22d8132e95347b9352a6ed
aruneshguleria9 <42490325+aruneshguleria9@users.noreply.github.com>
2018-08-27 15:09:55 +0530

    Update assg-6.py

--- assg-6.py
+++ assg-6.py
@@ -29,6 +29,23 @@ def pow(a,b):
     else:
         return(a*pow(a,b-1))
 print(pow(a,b))
+#old link submutted by mistake so thats why merging the two files
+#assignment7.
+#ques_1.
+age=int(input("enter age to find "))
+l={'ashu':20,'aru':21,'bidu':19,'papu':22}
+for k,j in l.items():
+    if j==age:
+     print(k)
+#ques_2.
+students = {'ash': {'english': 20, 'maths': 40, 'science': 60},
+          'jeet': {'english': 30, 'maths': 50, 'science': 70},
+         'ashu': {'english': 89, 'maths': 78, 'science': 88},
+         'dawat': {'english': 45, 'maths': 55, 'science': 75}}
+name=input('enter name \n')
+for n,s in students.items():
+        if name==n:
+            print("\nName",n)
+            for marks in s:
+                print(marks+':',s[marks])
 
-
-    
